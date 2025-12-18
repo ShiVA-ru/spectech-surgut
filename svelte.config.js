a0d434c4949c02f0b639fd645b014340cc74ada1
Shinyaev Vladimir <secman@bk.ru>
2025-12-18 14:54:05 +0500

    change adapter from static to auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,8 @@
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
+import adapter from '@svelte/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const dev = process.argv.includes('dev');
+// const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
